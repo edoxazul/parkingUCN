2be47c8751aecae46aa26d7f652b93eb28253c62
Ignacio <fuenzalida.veas@gmail.com>
2020-07-12 21:58:44 -0400

    feat(parkingBackend/Program.cs): Implementation of HostBuilder

--- parkingBackend/Program.cs
+++ parkingBackend/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using ParkingUcn.ZeroIce;
+using ParkingUcn.ZeroIce.model;
 
-namespace parkingBackend
+namespace ParkingBackend.Server
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            return 0;
+        }
+
+
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host
+                .CreateDefaultBuilder(args)
+                // Development, Staging, Production
+                .UseEnvironment("Development")
+                // Logging configuration
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole(options =>
+                    {
+                        options.IncludeScopes = true;
+                        options.TimestampFormat = "[yyyyMMdd.HHmmss.fff]";
+                        options.DisableColors = false;
+                    });
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                // Enable Control+C listener
+                .UseConsoleLifetime()
+                // Service inside the DI
+                .ConfigureServices((hostContext, services) =>
+                {    
+                    // TheSystem
+                    services.AddSingleton<TheSystemDisp_, TheSystemImpl>();
+                    // Contratos
+                    services.AddSingleton<ContratosDisp_, ContratosImpl>();
+                    // The Fivet Context
+                    services.AddDbContext<ParkingContext>();
+                    // The FivetService
+                    services.AddHostedService<ParkingService>();
+                    // The logger
+                    services.AddLogging();
+    
+                    // Yhe wait 4 finish
+                    services.Configure<HostOptions>(option => 
+                    {
+                        option.ShutdownTimeout = System.TimeSpan.FromSeconds(15);
+                    });       
+                });
         }
     }
 }
