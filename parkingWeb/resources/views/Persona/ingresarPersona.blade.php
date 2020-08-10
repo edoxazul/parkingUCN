6895727fdc4105df5f3434c189a961319d78ef8d
Eduardo <edualvsald@hotmail.com>
2020-08-10 01:26:16 -0400

    refactor(InsertarPersona): fixed view

--- parkingWeb/resources/views/Persona/ingresarPersona.blade.php
+++ parkingWeb/resources/views/Persona/ingresarPersona.blade.php
@@ -14,11 +14,12 @@
         <div class="col-md-12 justify-content-center">
             <div class="card border-secondary shadow">
                 <div class="card-header h2 d-flex justify-content-center mb-4 bg-tertiary">
-                    <div class="card-body">
-                        <form action="{{action('PersonaController@insertar')}}" method="POST">
-                            @include('Persona.partials.form')
-                        </form>
-                    </div>
+                    Ingresar Persona
+                </div>
+                <div class="card-body">
+                    <form action="{{action('PersonaController@insertar')}}" method="POST">
+                        @include('Persona.partials.ingresar')
+                    </form>
                 </div>
             </div>
         </div>
