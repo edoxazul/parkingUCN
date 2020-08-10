ddbc4932a0bc859411a201b796bb59773c6f7b9d
Eduardo <edualvsald@hotmail.com>
2020-08-10 02:05:54 -0400

    feat(InsertarVehiculo): implemented method Insertar Vehiculo

--- parkingWeb/app/Http/Controllers/VehiculoController.php
+++ parkingWeb/app/Http/Controllers/VehiculoController.php
@@ -5,27 +5,83 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use http\Url;
 use model\Persona;
+
 require_once 'Ice.php';
 //for fixed dir from domain.php
-require_once  base_path()."./../domain.php";
+require_once base_path() . "./../domain.php";
 
 class VehiculoController extends Controller
 {
 
-    public function ingresarView(){
+    public function ingresarView()
+    {
         return view('Vehiculo.ingresarVehiculo');
 
     }
 
+    public function insertar(Request $request)
+    {
+        // Data Request
+        $patente = $request->input("patente");
+        $marca = $request->input("marca");
+        $modelo = $request->input("modelo");
+        $anio = $request->input("anio");
+        $observaciones = $request->input("observaciones");
+        $runDuenio = $request->input('runDuenio');
+        $location = $request->input('location');
+
+        $locationVal = 0;
 
 
-    public function eliminarView(){
+        // ZeroIce
+        $ice = null;
+        $theSystem = null;
+
+        try {
+            $ice = \Ice\Initialize();
+            $proxy = $ice->stringToProxy("TheSystem:default -p 4020");
+            $theSystem = \model\TheSystemPrxHelper::checkedCast($proxy);
+
+            //Verification of sex;
+            if ($location == 'IN') {
+                $locationVal = 0;
+            } elseif ($location == 'OUT') {
+                $locationVal = 1;
+            } else {
+                $locationVal = 2;
+            }
+
+
+            $vehiculo = new Vehiculo();
+            $vehiculo->patente = $patente;
+            $vehiculo->marca = $marca;
+            $vehiculo->modelo = $modelo;
+            $vehiculo->anio = $anio;
+            $vehiculo->observaciones = $observaciones;
+            $vehiculo->runDuenio = $runDuenio;
+            $vehiculo->location = $locationVal;
+
+            $vehiculo = $theSystem->registrarVehiculo($vehiculo);
+            // The rut not exist in database
+            if ($vehiculo == null) {
+                return redirect()->back()->with('alert', 'Error al añadir el vehiculo');
+            }
+            if ($ice) {
+                $ice->destroy();
+            }
+            return redirect()->back()->with('success', 'Vehiculo Añadido Correctamente!');
+        } catch (Exception $ex) {
+            return redirect()->back()->with('alert', 'Error al agregar!');
+        }
+    }
+
+    public function eliminarView()
+    {
         return view('Vehiculo.eliminarVehiculo');
     }
 
-
-
-    public function eliminar(Request $request){
+    public function eliminar(Request $request)
+    {
 
         // Data Request
         $patente = $request->input("patente");
@@ -47,14 +103,14 @@ class VehiculoController extends Controller
             $theSystem = \model\TheSystemPrxHelper::checkedCast($proxy);
 
             // Elimination of person
-            $vehiculoBackend =  $theSystem->eliminarVehiculo($patente);
+            $vehiculoBackend = $theSystem->eliminarVehiculo($patente);
 
             // The rut not exist in database
             if ($vehiculoBackend == null) {
                 return redirect()->back()->with('alert', 'Vehiculo No Encontrado!');
             } //TODO: Verificar backend no responde
 
-            if ($ice){
+            if ($ice) {
                 $ice->destroy();
             }
 
